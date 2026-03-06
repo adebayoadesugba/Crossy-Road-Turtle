3efa12d1b6a79698359d6a0bda8f020b397ec3d4
Adebayo Adesugba <adebayoadesugba2002@gmail.com>
2026-03-06 01:38:49 +0100

    Implement game pause feature
    Added pause functionality to the game, allowing players to pause and resume gameplay. Updated scoreboard display during pause state.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard
+from scoreboard import Scoreboard, PauseScreen
 import random
 screen = Screen()
 screen.setup(width=1000, height=600)
@@ -11,27 +11,32 @@ screen.listen()
 
 
 
-
+pause = PauseScreen()
 player = Player()
 scores = Scoreboard()
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 lanes = [-230, -200, -170, -140, -110, -80, -50, -20, 10, 40, 70, 100, 130, 160, 190, 220]
 screen.onkeypress(player.move, "Up")
 screen.onkeypress(player.move, "w")
+
 def pause_game():
         global game_paused
         game_paused = not game_paused
+
 all_car = []
+
 game_paused = False
 
 screen.onkeypress(pause_game, "p")
+
 game_is_on = True
 while game_is_on:
     time.sleep(scores.car_speed)
     screen.update()
         
     if not game_paused:
-       
+        scores.goto(0, 250)
+        scores.write("Press 'p' to pause", align="center", font=("Arial",12,"bold"))
 
         
         new_car = CarManager()
@@ -39,13 +44,7 @@ while game_is_on:
         new_car.color(random.choice(COLORS))
         new_car.goto(x=520, y=random.choice(lanes))
 
-        if game_paused:
-            scores.goto(0,0)
-            scores.write("PAUSED", align="center", font=("Arial",30,"bold"))
-        else:
-            scores.clear()
-            scores.update_scoreboard()
-            scores.update_lives()
+      
         
         if player.ycor() > 290:
             player.reset_position()
@@ -68,7 +67,9 @@ while game_is_on:
                 game_is_on = False
 
     if game_paused:
-        scores.goto(0,0)
-        scores.write("PAUSED", align="center", font=("Arial",30,"bold"))   
+        pause.pause()
+    else:
+        pause.clear()
+         
         print(scores.car_speed)  
 screen.exitonclick()
